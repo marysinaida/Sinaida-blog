1d15c2ae88450613cee732e736cf9f58a06f1e59
marysinaida <marydorcassinaida54@gmail.com>
2019-11-30 14:13:54 +0300

    remmoved number2

--- myblog.py
+++ myblog.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Jane Nyambura',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Prissilah kimberline',
+        'title': 'Blog Post ',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
