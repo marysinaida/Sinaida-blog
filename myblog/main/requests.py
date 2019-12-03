19fa3937a41a3fda0db4b0b8e8bea4c9dae9a050
marysinaida <marydorcassinaida54@gmail.com>
2019-12-03 15:36:09 +0300

    add a funtion that initialize our app

---
+++ myblog/main/requests.py
@@ -0,0 +1,14 @@
+# import urllib.request
+# import json
+# from .user import Quote
+
+# base_url = None
+
+
+# def get_quote():
+#     url="http://quotes.stormconsultancy.co.uk/random.json (Links to an external site.)"
+#     res = requests.get(url)
+#     res_json = res.json()
+#     print(res_json)
+
+#     return res_json
