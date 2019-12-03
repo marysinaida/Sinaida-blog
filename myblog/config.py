779bd3740af606fa42d44daa7c5c0e2eeb242d27
marysinaida <marydorcassinaida54@gmail.com>
2019-12-03 07:28:22 +0300

    removed dummy data

--- myblog/config.py
+++ myblog/config.py
@@ -1,57 +1,56 @@
-# import os
+#  import os
 
-# class Config:
+#  class Config:
 #     '''
 #     General configuration parent class
 #     '''
 #     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:@Sopiti1221@localhost/pitch_test'
 #     SECRET_KEY = os.environ.get('SECRET_KEY')
 #     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:@Sopiti1221@localhost/blog'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+#    SQLALCHEMY_TRACK_MODIFICATIONS = False
+#    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
-#     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
+# #     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
 
-#     #simple mde configurations
-#     SIMPLEMDE_JS_IIFE = True
-#     SIMPLEMDE_USE_CDN = True
+# #     #simple mde configurations
+# #     SIMPLEMDE_JS_IIFE = True
+# #     SIMPLEMDE_USE_CDN = True
+# # # email configuration
+# #   MAIL_SERVER = 'smtp.googlemail.com'
+# #   MAIL_PORT = 587
+# #   MAIL_USE_TLS = True
+# #   MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+# #   MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+# #   SUBJECT_PREFIX = 'blog'
+# #   SENDER_EMAIL = 'marydorcassinaida54@gmail.com'
 
-#     #email configuration
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-#     SUBJECT_PREFIX = 'blog'
-#     SENDER_EMAIL = 'titusouko@gmail.com'
+# # #     @staticmethod
+# #     def init_app(app):
+# #         pass
 
-#     @staticmethod
-#     def init_app(app):
-#         pass
-
-# class ProdConfig(Config):
-#     '''
-#     Production configuration child class
-#     Args:
-#         Config: The parent configuration class with general configuration settings
-#     '''
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+# # class ProdConfig(Config):
+# #     '''
+# #     Production configuration child class
+# #     Args:
+# #         Config: The parent configuration class with general configuration settings
+# #     '''
+# #     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
 
-# class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:@Sopiti1221@localhost/blog'
+# # class TestConfig(Config):
+# #     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:@Sopiti1221@localhost/blog'
 
-# class DevConfig(Config):
-#     '''
-#     Development configuration child class
-#     Args:
-#         Config: The parent configuration class with general configuration settings
-#     '''
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:@Sopiti1221@localhost/blog'
-#     DEBUG = True
+# # class DevConfig(Config):
+# #     '''
+# #     Development configuration child class
+# #     Args:
+# #         Config: The parent configuration class with general configuration settings
+# #     '''
+# #     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:@Sopiti1221@localhost/blog'
+# #     DEBUG = True
 
-# config_options = {
-#     'development':DevConfig,
-#     'production':ProdConfig,
-#     'test': TestConfig
+# # config_options = {
+# #     'development':DevConfig,
+# #     'production':ProdConfig,
+# #     'test': TestConfig
 # }
