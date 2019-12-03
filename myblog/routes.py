aa13166ab333cfcda9a90dcffb2f536e0de013d0
marysinaida <marydorcassinaida54@gmail.com>
2019-12-03 08:14:21 +0300

    import forms

--- myblog/routes.py
+++ myblog/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect,request
 from myblog import app,bcrypt,db
-from myblog.forms import RegistrationForm, LoginForm, UpdateAccountForm,PostForm
+from myblog.forms import RegistrationForm, LoginForm, UpdateAccountForm,PostForm, RequestResetForm, ResetPasswordForm
 from myblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
